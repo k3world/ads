21ac168d00791f3d364232a590057cf8b9dc5630
k3world <34658008+k3world@users.noreply.github.com>
2017-12-18 23:46:57 +0600

    Add files via upload

---
+++ jubna.js
@@ -0,0 +1,20 @@
+<div id="jubna3676"></div>
+<script type="text/javascript">
+(function() {
+var params =
+{
+id: "810b5523-77f58eda-679b845e-7846d0b5",
+d: "YWJvdWRjcm0uY29t",
+cb: ((new Date()).valueOf().toString())
+}; 
+var qs="";
+for(var key in params){qs+=key+"="+params[key]+"&"}
+qs=qs.substring(0,qs.length-1);
+var s = document.createElement("script");
+s.type= "text/javascript";
+s.setAttribute("data-cfasyn", "false");
+s.src = "https://jubna.com/ar/api/widget/3676?" + qs;
+s.async = true;
+document.getElementById("jubna3676").appendChild(s);
+})();
+</script>
